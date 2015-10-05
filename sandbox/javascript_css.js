93fffd82da49c9a548a3682911bcee6edc74c9cb
Samuel Hibbard <hibbards01@gmail.com>
2015-10-05 13:53:49 -0600

    Almost done with javascript and css.

--- sandbox/javascript_css.js
+++ sandbox/javascript_css.js
@@ -4,7 +4,33 @@
 *************************************/
 
 window.onload = function() {
-    document.getElementById('move').onclick = function(event) {
-        this.className = 'move move-animation';
+    // On click event.
+    document.getElementById('js1').onclick = function(event) {
+        // Check the class name
+        if (this.className.indexOf("box-animate") != -1) {
+            this.className = "box";
+        } else {
+            this.className = this.className + " box-animate";
+        };
     };
+
+    // Now for a onmouseover event
+    document.getElementById('js2').onmouseover = function(event) {
+        this.className = "move1 moving";
+    };
+
+    // onmouseout
+    document.getElementById('js2').onmouseout = function(event) {
+        this.className = 'move1';
+    };
+
+    // onkeydown
+    document.getElementById('js3').onkeydown = function(event) {
+        // Add some text to the input
+        document.getElementById('js4').className = 'show';
+    };
+
+    document.getElementById('js3').onchange = function(event) {
+        document.getElementById('js4').className = 'hide';
+    }
 };
