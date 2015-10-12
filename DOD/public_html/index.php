d37d844163936d9d380a20cc658e3a7aee4f2a32
cschunsiu <cs_chunsiu@yahoo.com.hk>
2015-10-12 15:07:39 -0600

    sandbox code for HTML tags

---
+++ DOD/public_html/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <title>Home</title>
+    <meta name="description" content="football,NFL">
+    <meta name="keywords" content="football">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="/css/screen.css" rel="stylesheet" type="text/css" media="screen">
+    <body>
+        <div>
+            <header>
+            </header>
+            <main>
+                <div class="mainContent">
+                <h2>FOOTBALL!!!!!!!!!!!!!!!!!!!</h2>
+                <audio controls>
+                    <source src="/audio/foodIntro.mp3" type="audio/mp3">
+                    <source src="/audio/foodIntro.ogg" type="audio/ogg">
+                    <p>browser needs to be upgraded</p>
+                </audio>
+                <br>
+                <div class="video">
+                    <iframe width="400" height="350" src="https://youtu.be/-lGrARvsuD0" allowfullscreen></iframe> 
+                </div>                   
+                <footer>
+                    <script src="/javascript/analytics.js"></script>
+                    &copy;2014 cschunsiu<br>
+                </footer>                          
+                </div>  
+            </main>
+        </div>
+    </body>
+</html>
