34512fbff4c7eb366a654a111ae342c631eaec96
Samuel Hibbard <hibbards01@gmail.com>
2015-10-12 16:45:25 -0600

    This is for you Josh

--- sandbox/javascript_css.js
+++ sandbox/javascript_css.js
@@ -30,7 +30,33 @@ window.onload = function() {
         document.getElementById('js4').className = 'show';
     };
 
+    // onchange
     document.getElementById('js3').onchange = function(event) {
         document.getElementById('js4').className = 'hide';
     }
+
+    setInterval(changePics, 3000);
 };
+
+var index = 1;
+var img = null;
+
+function settup (argument) {
+    img = document.getElementById('img');
+}
+
+function changePics() {
+    // Grab the object
+    console.log("Incrementing");
+
+    // See if it is divisible by something
+    if (index % 2 == 0) {
+        img.src = "http://incitrio.com/wp-content/uploads/2015/01/Apple_gray_logo.png";
+    } else if (index % 3 == 0) {
+        img.src = "http://www.andrewkeir.com/wp-content/uploads/windows-8-logo-excerpt.jpg";
+    } else if (index % 5 == 0) {
+        img.src = "https://design.ubuntu.com/wp-content/uploads/logo-ubuntu_st_no%C2%AE-black_orange-hex.png";
+    };
+
+    index++;
+}
