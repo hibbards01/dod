9cef53592b705b9915525d43c152ae4e3e35f5f9
Samuel Hibbard <hibbards01@gmail.com>
2015-10-21 13:42:42 -0600

    Added the next part on my list.

--- sandbox/javascript_css.js
+++ sandbox/javascript_css.js
@@ -4,6 +4,10 @@
 *************************************/
 
 window.onload = function() {
+    //
+    // Manipulate CSS with javascript
+    //
+
     // Grab the onclick event
     document.getElementById("goodBox").onclick = function() {
         var box = this;
@@ -42,5 +46,47 @@ window.onload = function() {
         circle.style.backgroundColor = "#00000000000";
         circle.style.backgroundColor = "-FFF";
         circle.style.backgroundColor = "bob";
+    }
+
+    //
+    // Trigger animations and transitions using javascript
+    //
+    document.getElementById('goodBoxTransition').onclick = function() {
+        var box = this;
+
+        // Good path
+        console.log("goodBoxTransition good path");
+        box.style.transition = "all 2s linear";
+        box.style.height = "300px";
+        box.style.width = "400px";
+    }
+
+    document.getElementById('badBoxTransition').onclick = function() {
+        var box = this;
+
+        // Nasty path
+        console.log("badBoxTransition nasty path");
+        box.style.transition = "width height 2s ease";
+        box.style.height = "300px";
+        box.style.width = "400px";
+    }
+
+    document.getElementById('goodCircleAnimation').onclick = function() {
+        var circle = this;
+
+        // Good path
+        console.log("goodCircleAnimation good path");
+        circle.style.animation = "move 1s linear 2";
+        setTimeout(function() {circle.style.animation = "";}, 2000);
+    }
+
+    document.getElementById('badCircleAnimation').onclick = function() {
+        var circle = this.style;
+
+        // Nasty path
+        console.log("badCircleAnimation");
+        // circle.animation = "bob 2s linear 2";
+        circle.animation = "move 1 linear 2";
+        setTimeout(function() {circle.animation = "";}, 2000);
     }
 };
