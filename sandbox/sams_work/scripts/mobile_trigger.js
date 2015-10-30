cbdbcd58f6c2a75ff2b3034dfea8cc470bbbc732
Samuel Hibbard <hibbards01@gmail.com>
2015-10-30 14:25:24 -0600

    Working with events.

--- sandbox/sams_work/scripts/mobile_trigger.js
+++ sandbox/sams_work/scripts/mobile_trigger.js
@@ -8,13 +8,51 @@
 *       events.
 ****************************************************/
 
-function init() {
-    // Grab moving
+var offsetX = 0;
+var offsetY = 0
 
-    // Grab click event
-    document.getElementById('moveCircle').ontouchstart = function(event) {
-        console.log("HERE!");
-    }
+window.onload = function() {
+    //
+    // Good Paths
+    //
+
+    // Grab the button
+    document.getElementById('button').onclick = doSomething;
+}
+
+//
+// Good Paths
+//
+function init() {
+    // Grab touch event
+    var circle = document.getElementById('moveCircle');
+    circle.addEventListener("touchstart", handleStart, false);
+    circle.addEventListener("touchmove", handleMove, false);
+}
+
+function handleStart(event) {
+    // Grab the position
+    var position = document.getElementById('moveCircle').getBoundingClientRect();
+
+    // Grab the offest
+    offsetX = event.changedTouches[0].pageX - position.left;
+    offsetY = event.changedTouches[0].pageY - (position.top - 50);
+}
+
+function handleMove(event) {
+    // Grab the div tag.
+    var circle = document.getElementById('moveCircle');
+    var top = event.changedTouches[0].pageY - offsetY;
+    var left = event.changedTouches[0].pageX - offsetX;
+    circle.style.top = top + 'px';
+    circle.style.left = left + 'px';
+}
+
+function doSomething(event) {
+    var body = document.getElementById('body');
+    var h1 = document.createElement('h1');
+    h1.innerHTML = 'It worked!';
+    body.appendChild(h1);
 }
 
 function moving(event) {
@@ -22,3 +60,7 @@ function moving(event) {
         console.log(key);
     }
 }
+
+//
+// Nasty Paths
+//
